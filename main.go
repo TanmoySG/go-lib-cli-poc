d147194a6459191aadb00af43dabdca2e95c8c01
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2023-01-19 19:54:20 +0000

    test2

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+
+	"github.com/TanmoySG/go-lib-cli-poc/internal/mathctlFuncs"
+)
+
+func main() {
+
+	app := cli.NewApp()
+	app.Name = "Website Lookup CLI"
+	app.Usage = "Let's you query IPs, CNAMEs, MX records and Name Servers!"
+
+	addFlags := []cli.Flag{
+		&cli.StringFlag{
+			Name:        "n1",
+			Value:       "english",
+			Usage:       "to do",
+		},
+		&cli.StringFlag{
+			Name:        "n2",
+			Value:       "english",
+			Usage:       "to do",
+		},
+	}
+
+	app.Commands = []*cli.Command{
+		{
+			Name:  "add",
+			Flags: addFlags,
+			Action: mathctlFuncs.Add,
+		},
+		{
+			Name: "sub",
+			Flags: addFlags,
+			Action: mathctlFuncs.Sub,
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
